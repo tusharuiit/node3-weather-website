3e6e4f9c5d383bcbdb7abf5bbfd9150a8856fb0d
Tushar Upadhyay <tusharuiit@gmail.com>
2020-04-14 01:16:36 +0200

    priting port to console

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const forecast = require('./utils/forecast.js')
 const app = express()
 const port = process.env.PORT || 3000
 
+console.log('port value is ' + port)
+
 //  Define Paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
